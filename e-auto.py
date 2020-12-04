37d639f31ea88dce43dab4ef275f9ae02f20a67c
yugu0202 <applerin1119@gmail.com>
2020-12-05 01:31:37 +0900

    Consideration setup.py

--- e-auto.py
+++ e-auto.py
@@ -10,7 +10,7 @@ import re
 basePath = os.path.split(os.path.realpath(__file__))[0]
 
 #ユーザー情報の入力待機
-chromedriver_path = "D:\download\chrome\chromedriver.exe"#input("chromedriverpath>")#Chromedriverのディレクトリパス
+chromedriver_path = os.path.join(basePath,"lib/chromedriver.exe")#Chromedriverのディレクトリパス
 user_id = input("id>")#e-LeaningのID
 user_pass = getpass.getpass("pass>")#e-Leaningのパスワード
 
